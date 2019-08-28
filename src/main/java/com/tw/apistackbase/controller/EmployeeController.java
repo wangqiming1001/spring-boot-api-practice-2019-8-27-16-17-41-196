3cce76c2ff67f0248d4e28a4785a129cbdabaa92
wangqiming1001 <512821390@qq.com>
2019-08-28 23:33:26 +0800

    company photo and employee

--- src/main/java/com/tw/apistackbase/controller/EmployeeController.java
+++ src/main/java/com/tw/apistackbase/controller/EmployeeController.java
@@ -4,8 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tw.apistackbase.model.Employee;
@@ -22,6 +28,40 @@ public class EmployeeController {
 	@GetMapping
 	public ResponseEntity<List<Employee>> queryEmployees(){
 		return ResponseEntity.ok(employeeService.queryEmployees());
+	}
+	
+	//获得某个特定员工
+	@GetMapping("/{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable int id) {
+		return ResponseEntity.ok(employeeService.getEmployeeById(id));
+	}
+	//分页查询
+	
+	//查询所有男性员工
+	@GetMapping("/gender")
+	public ResponseEntity<List<Employee>> getEmployeesByGender(@RequestParam("gender") String gender) {
+		List<Employee> maleEmployeeList = employeeService.getEmployeesByGender(gender);
+		return ResponseEntity.ok(maleEmployeeList);
+	}
+	
+	//添加一名员工
+	@PostMapping("")
+	public ResponseEntity<List<Employee>> createEmployee(@RequestBody Employee employee){
+		return ResponseEntity.ok(employeeService.createEmployee(employee));
+	}
+	
+	//update一名员工
+	@PutMapping("/{id}")
+	public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee, @PathVariable int id){
+		Employee newEmployee = employeeService.updateEmployee(employee, id);
+		return ResponseEntity.ok(newEmployee);
+	}
+	
+	//delete一名员工
+	@DeleteMapping("/{id}")
+	public ResponseEntity<List<Employee>> deleteEmployee(@PathVariable int id){
+		List<Employee> employee = employeeService.deleteEmployee(id);
+		return ResponseEntity.ok(employee);
 	}
 	
 }
--- src/main/java/com/tw/apistackbase/service/EmployeeService.java
+++ src/main/java/com/tw/apistackbase/service/EmployeeService.java
@@ -1,5 +1,6 @@
 package com.tw.apistackbase.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,58 @@ public class EmployeeService {
 	public List<Employee> queryEmployees(){
 		List<Employee> employee = dataBase.getEmployees();
 		return employee;
+	}
+	
+	public Employee getEmployeeById(int id) {
+		List<Employee> Employees = dataBase.getEmployees();
+		Employee employeer = new Employee();
+		for(Employee employee : Employees) {
+			if(employee.getId() == id) {
+				employeer = employee;
+			}
+		}
+		return employeer;
+	}
+	
+	public List<Employee> getEmployeesByGender(String gender) {
+		List<Employee> result = new ArrayList<Employee>();
+		List<Employee> Employees = dataBase.getEmployees();
+		for (int i = 0; i < Employees.size(); i++) {
+			if(gender.equals(Employees.get(i).getGender())) {
+				result.add(Employees.get(i));
+			}
+		}
+		return result;
+	}
+	
+	public List<Employee> createEmployee(Employee employee){
+		dataBase.getEmployees().add(employee);
+		List<Employee> employeeList = dataBase.getEmployees();
+		return employeeList;
+	}
+	
+	public Employee updateEmployee(Employee employee,int id){
+		Employee result = new Employee();
+		for(Employee e : dataBase.getEmployees()) {
+			if(e.getId() == id) {
+				e.setAge(employee.getAge());
+				e.setName(employee.getName());
+				e.setGender(employee.getGender());
+				e.setSalary(employee.getSalary());
+				result = e;
+			}
+		}
+		return result;
+	}
+	
+	public List<Employee> deleteEmployee(int id){
+		List<Employee> employeeList = dataBase.getEmployees();
+		for (int i = 0; i < employeeList.size(); i++) {
+			if (employeeList.get(i).getId() == id) {
+				employeeList.remove(i);
+			}
+		}
+		return employeeList;
 	}
 	
 }
