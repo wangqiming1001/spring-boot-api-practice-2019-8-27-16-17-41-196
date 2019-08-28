b40876e987c319241795e7fb66b34a63617bc9e0
wangqiming1001 <512821390@qq.com>
2019-08-28 22:24:44 +0800

    company API commit

--- src/main/java/com/tw/apistackbase/controller/CompanyController.java
+++ src/main/java/com/tw/apistackbase/controller/CompanyController.java
@@ -4,12 +4,17 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tw.apistackbase.model.Company;
+import com.tw.apistackbase.model.Employee;
 import com.tw.apistackbase.service.CompanyService;
 
 @RestController
@@ -29,7 +34,37 @@ public class CompanyController {
 	@GetMapping("/{id}")
 	public ResponseEntity<Company> queryCompanyById(@PathVariable int id) {
 		Company company = companyService.queryCompanyById(id);
-				return ResponseEntity.ok(company);
+		return ResponseEntity.ok(company);
 	}
+	
+	//获取某个公司下的所有员工
+	@GetMapping("/{id}/employees")
+	public ResponseEntity<List<Employee>> getEmployeesByCompanyId(@PathVariable int id) {
+		List<Employee> employees = companyService.getEmployeesById(id);
+		return ResponseEntity.ok(employees);
+	}
+	
+	//分页查询
+	
+	//添加一个公司
+	@PostMapping("")
+	public ResponseEntity<List<Company>> createCompany(@RequestBody Company company){
+		List<Company> companies = companyService.createCompany(company);
+		return ResponseEntity.ok(companies);
+	}
+	
+	//更新一个公司的基本信息
+	@PutMapping("/{id}")
+	public ResponseEntity<Company> updateCompany(@RequestBody Company company, @PathVariable int id){
+		Company newCompany = companyService.updateCompany(company,id);
+		return ResponseEntity.ok(newCompany);
+	}
+	
+	//删除某个特定的公司
+	@DeleteMapping("/{id}")
+	public ResponseEntity<List<Company>> deleteCompany(@PathVariable int id){
+		return ResponseEntity.ok(companyService.deleteCompanyById(id));
+	}
+	
 	
 }
--- src/main/java/com/tw/apistackbase/service/CompanyService.java
+++ src/main/java/com/tw/apistackbase/service/CompanyService.java
@@ -3,11 +3,11 @@ package com.tw.apistackbase.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.tw.apistackbase.model.Company;
+import com.tw.apistackbase.model.Employee;
 import com.tw.apistackbase.util.DataBase;
 
 @Service
@@ -28,5 +28,42 @@ public class CompanyService {
 			}
 		}
 		return null;
+	}
+	
+	public List<Employee> getEmployeesById(@PathVariable int id) {
+		for(Company company : dataBase.getCompanies()) {
+			if(company.getId() == id) {
+				List<Employee> employees =  company.getEmployees();
+				return employees;
+			}
+		}
+		return null;
+	}
+	
+	public List<Company> createCompany(Company company){
+		dataBase.getCompanies().add(company);
+		List<Company> companies = dataBase.getCompanies();
+		return companies;
+	}
+	
+	public Company updateCompany(Company company,int id){
+		for(Company c : dataBase.getCompanies()) {
+			if(c.getId() == id) {
+				c.setCompanyName(company.getCompanyName());
+				c.setEmployees(company.getEmployees());
+				return company;
+			}
+		}
+		return null;
+	}
+	
+	public List<Company> deleteCompanyById(int id){
+		List<Company> companyList = dataBase.getCompanies();
+		for (int i = 0; i < companyList.size(); i++) {
+			if (companyList.get(i).getId() == id) {
+				companyList.remove(i);
+			}
+		}
+		return companyList;
 	}
 }
